35075cf934e66ceba2dec342c7ff52c9aa8e3e99
Gerardpedrero <gerardpedrero2002@ginebro.cat>
2021-02-04 18:04:24 +0100

    Haaaaaaaaaaaaaaachus

---
+++ sp03_Llibres_Autor.sql
@@ -0,0 +1,21 @@
+
+USE LLIBRERIA;
+
+
+DELIMITER //
+
+
+DROP PROCEDURE IF EXISTS sp02_Llibres_Autor//
+
+CREATE PROCEDURE sp02_Llibres_Autor(
+      IN  p_autor varchar(30))
+  BEGIN
+
+    SELECT TITOL, EDITORIAL, PREU
+    FROM   LLIBRES
+    WHERE  AUTOR = p_autor;
+
+
+  END//
+
+DELIMITER ;
