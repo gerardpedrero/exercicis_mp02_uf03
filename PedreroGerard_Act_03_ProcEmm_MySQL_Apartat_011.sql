9c3953f5ce1ba7245459ce9425e03f3c254674f9
gerardpedrero <72878076+gerardpedrero@users.noreply.github.com>
2021-04-19 15:09:10 +0200

    Update PedreroGerard_Act_03_ProcEmm_MySQL_Apartat_011.sql

---
+++ PedreroGerard_Act_03_ProcEmm_MySQL_Apartat_011.sql
@@ -0,0 +1,21 @@
+USE videoclub;
+DROP FUNCTION IF EXISTS act11;
+
+DELIMITER //
+CREATE FUNCTION act11(peCodiPeli SMALLINT UNSIGNED) 
+       RETURNS SMALLINT UNSIGNED
+       DETERMINISTIC
+BEGIN
+   DECLARE QtatExemplars SMALLINT UNSIGNED;
+
+   SELECT   COUNT(*)
+        INTO QtatExemplars
+   FROM     EXEMPLARS
+   WHERE    id_peli = peCodiPeli;
+
+   RETURN QtatExemplars;
+END//
+DELIMITER ;
+  SELECT  titol_peli Titol, act11(1) "Quantitat exemplars"
+   FROM    PELLICULES
+   WHERE   id_peli = 1;
