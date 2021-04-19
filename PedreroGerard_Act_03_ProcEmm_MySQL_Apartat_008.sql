aa030c26c700293a6cfe325c629369c55d018096
gerardpedrero <72878076+gerardpedrero@users.noreply.github.com>
2021-04-19 14:58:13 +0200

    Activitat 8

---
+++ PedreroGerard_Act_03_ProcEmm_MySQL_Apartat_008.sql
@@ -0,0 +1,39 @@
+USE videoclub;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS titolAndRendibilitat//
+
+CREATE PROCEDURE titolAndRendibilitat(
+IN codi_peli smallint)
+BEGIN
+
+DECLARE recaudacio float DEFAULT 0;
+DECLARE pressupost float DEFAULT 0;
+DECLARE nivellrecaudacio varchar(12);
+
+SELECT titol_peli
+FROM PELLICULES
+WHERE id_peli = codi_peli;
+
+SELECT recaudacio_peli INTO recaudacio
+FROM PELLICULES
+WHERE id_peli = codi_peli;
+
+SELECT pressupost_peli INTO pressupost
+FROM PELLICULES
+WHERE id_peli = codi_peli;
+
+IF recaudacio < pressupost THEN
+SET nivellrecaudacio = "Deficitari";
+ELSEIF recaudacio < (pressupost * 2) THEN
+SET nivellrecaudacio = "Suficient";
+ELSEIF recaudacio > (pressupost * 2) THEN
+SET nivellrecaudacio = "Bona";
+END IF;
+SELECT nivellrecaudacio;
+END //
+
+DELIMITER ;
+
+CALL titolAndRendibilitat(1);
