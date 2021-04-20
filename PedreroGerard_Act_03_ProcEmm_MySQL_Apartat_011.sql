6cf6b6228aeb3f1e4f389210bd4539caf9d28be5
gerardpedrero <72878076+gerardpedrero@users.noreply.github.com>
2021-04-20 18:09:13 +0200

    Activitat 11

--- PedreroGerard_Act_03_ProcEmm_MySQL_Apartat_011.sql
+++ PedreroGerard_Act_03_ProcEmm_MySQL_Apartat_011.sql
@@ -7,13 +7,34 @@ CREATE FUNCTION act11(CodiPeli SMALLINT UNSIGNED)
        RETURNS Smallint
        DETERMINISTIC
 BEGIN
-   DECLARE QuanExem Smallint UNSIGNED;
+   DECLARE QuanVis Smallint UNSIGNED;
 
    select   COUNT(*)
-        into QuanExem 
+        into QuanVis 
    FROM     VISUALITZACIONS
    WHERE    id_peli = CodiPeli;
 
-   RETURN QuanExem;
+   RETURN QuanVis;
 END//
 DELIMITER ;
+
+ Select titol_peli, act11(1) 
+ FROM    PELLICULES
+WHERE   id_peli = 1;
+
+
+
+CREATE TABLE VISUALITZACIONS(
+    id_peli	  SMALLINT NOT NULL,
+    id_soci	 SMALLINT NOT NULL,
+    temps      VARCHAR(8) NOT NULL PRIMARY KEY
+);
+
+
+
+
+Insert into VISUALITZACIONS(id_soci,id_peli,temps)
+Values (1,5,"02:01:01"),
+ (1,2,"03:02:01"),
+ (1,3,"04:03:01"),
+ (1,4,"05:04:01");
